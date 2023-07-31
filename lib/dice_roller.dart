5d12b7f374dc888d6d7455772f2d7c27718a5308
tanush077 <tanushgolwala@gmail.com>
2023-07-31 17:48:28 +0530

    first

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -21,19 +21,22 @@ class dicerollerstate extends State<Diceroller> {
 
   @override
   Widget build(context) {
-    return Column(mainAxisSize: MainAxisSize.min, children: [
-      Image.asset(
-        'assets/images/dice-$currentdiceroll.png',
-        width: 200,
-      ),
-      TextButton(
-        onPressed: rolldice,
-        style: TextButton.styleFrom(
-            padding: const EdgeInsets.all(10),
-            foregroundColor: Colors.white,
-            textStyle: const TextStyle(fontSize: 28)),
-        child: const Text('Roll Dice'),
-      )
-    ]);
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentdiceroll.png',
+          width: 200,
+        ),
+        TextButton(
+          onPressed: rolldice,
+          style: TextButton.styleFrom(
+              padding: const EdgeInsets.all(10),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 28)),
+          child: const Text('Roll Dice'),
+        )
+      ],
+    );
   }
 }
